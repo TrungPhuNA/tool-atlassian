4b370cfb31e8195f34d77c07a69c1c9d17f15d9a
phuphan <codethue94@gmail.com>
2026-07-14 08:36:51 +0700

    fixx

--- api/migrations/20260525150000-add-needs-solution-discussion.js
+++ api/migrations/20260525150000-add-needs-solution-discussion.js
@@ -2,16 +2,22 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('jira_issues', 'needs_solution_discussion', {
-      type: Sequelize.BOOLEAN,
-      defaultValue: false,
-      allowNull: false,
-      after: 'has_due_date',
-      comment: 'Có cần trao đổi giải pháp hay không'
-    });
+    const table = await queryInterface.describeTable('jira_issues');
+    if (!table.needs_solution_discussion) {
+      await queryInterface.addColumn('jira_issues', 'needs_solution_discussion', {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+        after: 'has_due_date',
+        comment: 'Có cần trao đổi giải pháp hay không'
+      });
+    }
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('jira_issues', 'needs_solution_discussion');
+    const table = await queryInterface.describeTable('jira_issues');
+    if (table.needs_solution_discussion) {
+      await queryInterface.removeColumn('jira_issues', 'needs_solution_discussion');
+    }
   }
 };
